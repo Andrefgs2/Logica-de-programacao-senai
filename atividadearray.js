180951d3fb226863c0a92a2d70200bfdc6907065
ANDRÉ SAMUEL COSMO FAGUNDES <andre_sc_fagundes@estudante.sesisenai.org.br>
2026-03-03 21:31:24 -0300

    atividadearray

---
+++ atividadearray.js
@@ -0,0 +1,86 @@
+// ============================================================
+//   ATIVIDADE 02 – Arrays (Listas) em JavaScript
+// ============================================================
+// Instruções: resolva cada exercício no espaço indicado.
+// Use console.log() para exibir os resultados.
+// ============================================================
+
+
+// ------------------------------------------------------------
+// EXERCÍCIO 1 – Criando e acessando arrays
+// ------------------------------------------------------------
+// a) Crie uma array chamada "cores" com pelo menos 4 cores.
+// b) Exiba a primeira e a última cor usando índices.
+
+// → Seu código aqui:
+let cores = ["azul", "vermelho", "verde", "amarelo"]
+console.log(cores[0])
+
+
+console.log("_______________________________");
+
+
+// ------------------------------------------------------------
+// EXERCÍCIO 2 – Métodos básicos
+// ------------------------------------------------------------
+// a) Adicione uma nova cor ao final de "cores", utilizando as funções de array.
+// b) Remova a primeira cor, utilizando as funções de array.
+// c) Exiba o array resultante e depois a quantidade de itens armazendos (length).
+
+// → Seu código aqui:
+cores.push("rosa")
+console.log(cores)
+cores.shift("azul")
+console.log(cores)
+console.log(cores.length)
+console.log(`quantidade de itens na lista cores: ${cores.length}`)
+console.log("_______________________________");
+
+
+// ------------------------------------------------------------
+// EXERCÍCIO 3 – Números
+// ------------------------------------------------------------
+// a) Crie um array de números.
+// b) Em uma única linha, exiba os valores armazenados nesta lista e a quantidade de números armazenados (quantidade de itens, não a soma)
+
+// → Seu código aqui:
+let nmr = [1, 2, 3, 4]
+console.log(nmr)
+console.log(nmr.length)
+console.log(`quantidade de numeros armazenados: ${nmr.length}`)
+
+console.log("_______________________________");
+
+
+// ------------------------------------------------------------
+// EXERCÍCIO 4 – Todos os tipos
+// ------------------------------------------------------------
+// a) Crie um array com todos os tipos de dados que você conhece (number, string...).
+// b) Em uma única linha, exiba os valores armazenados nesta lista e a quantidade de itens armazenados
+
+// → Seu código aqui:
+let todos = [1, "texto", true, null,undefined]
+
+console.log(todos)
+console.log(todos.length)
+console.log(`quantidade de itens armazenados: ${todos.length}`)
+
+// ------------------------------------------------------------
+// EXERCÍCIO 5 – Mudando a lista
+// ------------------------------------------------------------
+// a) Crie um array com vários dados de vários tipos diferentes.
+// b) Exiba a lista.
+// c) Remova o primeiro e o último item da lista, utilizando as funções de array.
+// d) Exiba a lista novamente.
+// e) Adicione um novo item no final e ou no início da lista.
+// f) Exiba a lista final.
+
+// → Seu código aqui:
+let final = [22, "yes", true, null]
+final.pop(null)
+final.shift(22)
+console.log(final)
+final.unshift(33)
+console.log(final)
+
+console.log("_______________________________");
---
+++ jp.js
@@ -0,0 +1,23 @@
+console.log(1+1)    //soma
+console.log("1"+"1") // concatenação
+
+console.log("hello world")
+let idade = 5;
+
+let linguagem = 'tudo bem'
+let templateString = `boa noite   ${linguagem} sim`
+console.log (templateString)
+
+let tempo = 'sol'
+let tempo2 = `hoje o ${tempo} esta lindo`
+console.log (tempo2)
+ let estaChovendo = false 
+ let estaSol = true 
+ console.log (estaChovendo)     // false
+console.log (estaSol)          //true
+console.log (typeof  estaSol)   //boolean
+  'undefined' = 'errado'
+  'null'  = 'intencionalmente vazio'
+  let aaa = null
+  console.log (aaa) 
+  console.log (tyoeof)
